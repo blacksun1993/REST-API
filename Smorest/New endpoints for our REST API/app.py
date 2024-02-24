9be55c0b88fb4063bbf49f9fe65867e23f88b364
blacksun1993 <oceanbluesky1993@gmail.com>
2024-02-24 11:38:06 +0800

    finished

---
+++ Smorest/New endpoints for our REST API/app.py
@@ -0,0 +1,141 @@
+from flask_smorest import abort
+from flask import Flask, request
+from db import stores, items
+import uuid
+
+app = Flask(__name__)
+
+#stores = [{"name": "My Store", "items": [{"name": "my item", "price": 15.99}]}]
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+@app.get("/store")
+def get_stores():
+    return {"stores": list(stores.values())}
+
+####################################################################################
+#store is a api for record 
+# ** means get all values from dict
+#input name
+#output store_id
+####################################################################################
+@app.post("/store")
+def create_store():
+    store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON payload.",
+        )
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
+
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+
+    return store
+
+####################################################################################
+#item is to generate item id
+# need to get store_id from /store first 
+#input store_id, name, price
+#output item_id
+####################################################################################
+@app.post("/item")  
+def create_item():
+    item_data = request.get_json()
+    # Here not only we need to validate data exists,
+    # But also what type of data. Price should be a float,
+    # for example.
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
+        )
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item
+####################################################################################
+#input store_id
+#output store_id, name
+####################################################################################
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        # Here you might also want to add the items in this store
+        # We'll do that later on in the course
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found.")
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="Item not found.")
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
+
+####################################################################################
+#put is a update function for parameters
+#input1 item_id on Url
+#input2 name, price, store_id
+#output updated name, price, store_id
+####################################################################################
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    # There's  more validation to do here!
+    # Like making sure price is a number, and also both items are optional
+    # You should also prevent keys that aren't 'price' or 'name' to be passed
+    # Difficult to do with an if statement...
+    if "price" not in item_data or "name" not in item_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
+        )
+    try:
+        item = items[item_id]
+        item |= item_data
+
+        return item
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted."}
+    except KeyError:
+        abort(404, message="Store not found.")
